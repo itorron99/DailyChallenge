6d15ae14e5e0e055e837d8bc87d467d0f81621e6
Iker <itorron@outlook.com>
2025-03-20 17:14:05 +0100

    chore: Refactor early return and fix !isNaN(value)

--- Day_1/index.js
+++ Day_1/index.js
@@ -4,14 +4,11 @@
 */
 
 export const prepareGifts = (gifts) => {
-  if (gifts && Array.isArray(gifts) && gifts.length > 0) {
-    return [
-      ...new Set(
-        gifts
-          ?.filter((gift) => typeof gift === "number" && !isNaN(value))
-          .sort()
-      ),
-    ];
-  }
-  return [];
+  if (!gifts || !Array.isArray(gifts) || !gifts.length === 0) return [];
+
+  return [
+    ...new Set(
+      gifts?.filter((gift) => typeof gift === "number" && !isNaN(gift)).sort()
+    ),
+  ];
 };
